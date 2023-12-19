f5b2b0514e162740ffd92bc837f471addbecf9dc
Ritesh Kumar <ritesh@emerald.pink>
2023-12-19 13:58:10 -0500

    Use external heex template for show_verse
    Just playing around with things...

--- lib/vyasa_web/live/gita_live/show_verse.ex
+++ lib/vyasa_web/live/gita_live/show_verse.ex
@@ -8,35 +8,6 @@ defmodule VyasaWeb.GitaLive.ShowVerse do
   end
 
   @impl true
-  def render(assigns) do
-    ~H"""
-    <.header>
-      <:subtitle><%= @verse.chapter_number %>:<%= @verse.verse_number %></:subtitle>
-      <p class="font-dn text-2xl"><%= @verse.text |> String.split("।।") |> List.first() %></p>
-    </.header>
-    <br />
-    <p><%= @verse.transliteration %></p>
-    <br />
-    <p><%= @verse.word_meanings %></p>
-    <br />
-    <.button
-    phx-hook="ShareQuoteButton"
-    id="ShareQuoteButton"
-    data-verse={Jason.encode!(@verse)}
-    data-share-title={"Gita Chapter #{@verse.chapter_number} #{@verse.title}"}
-    >
-      Share
-    </.button>
-
-    <.back navigate={~p"/gita/#{@verse.chapter_number}"}>
-      Back to Gita Chapter <%= @verse.chapter_number %>
-    </.back>
-    <.back navigate={~p"/gita"}>Back to Gita</.back>
-    """
-  end
-
-  # <.link patch={~p"/gita/#{@chapter.id}"} phx-click={JS.push_focus()}> <.button>Annotate</.button> </.link>
-  @impl true
   def handle_params(%{"chapter_id" => chapter_no, "verse_id" => verse_no}, _, socket) do
     {:noreply,
      socket
