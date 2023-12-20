fbc01629e62b718300c13dc3bc7b2df1fa2cf3ea
Ritesh Kumar <ritesh@emerald.pink>
2023-12-19 20:45:11 -0500

    Add meta tags to other views in gita_live

--- lib/vyasa_web/live/gita_live/show.ex
+++ lib/vyasa_web/live/gita_live/show.ex
@@ -8,27 +8,26 @@ defmodule VyasaWeb.GitaLive.Show do
   end
 
   @impl true
-  def render(assigns) do
-    ~H"""
-    <.header>
-      <%= @chapter.name_transliterated %>
-        <:subtitle> <%= @chapter.chapter_summary %></:subtitle>
-    </.header>
-
-    <.list :for={{_dom_id, text} <- @streams.verses}>
-        <:item title={"#{text.chapter_number}.#{text.verse_number}"}><p class="font-dn text-2xl"><%= text.text |> String.split("।।") |> List.first()  %></p></:item>
-        <:item><%= text.transliteration %></:item>
-        <:item><%= text.word_meanings %></:item>
-    </.list>
-
-    <.back navigate={~p"/gita"}>Back to Gita</.back>
-    """
-  end
-  #<.link patch={~p"/gita/#{@chapter.id}"} phx-click={JS.push_focus()}> <.button>Annotate</.button> </.link>
-  @impl true
   def handle_params(%{"chapter_id" => id}, _, socket) do
     {:noreply, socket
     |> assign(:chapter, Gita.chapters(id))
-    |> stream(:verses, Gita.verses(id))}
+    |> stream(:verses, Gita.verses(id))
+    |> assign_meta()}
+  end
+
+  defp assign_meta(socket) do
+    %{
+      :chapter_number => chapter,
+      :chapter_summary => summary,
+      :name => chapter_name,
+      :name_meaning => meaning,
+    } = socket.assigns.chapter
+
+    assign(socket, :meta, %{
+      title: "Chapter #{chapter} | #{chapter_name} -- #{meaning}",
+      description: summary,
+      type: "website",
+      url: url(socket, ~p"/gita/#{chapter}"),
+    })
   end
 end
