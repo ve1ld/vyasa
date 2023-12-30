8594bf371b5d49e0a1494c74c03af4861adf566e
Ritesh Kumar <ritesh@emerald.pink>
2023-12-30 18:07:35 -0500

    Fix text

--- test/vyasa_web/controllers/error_html_test.exs
+++ test/vyasa_web/controllers/error_html_test.exs
@@ -5,10 +5,10 @@ defmodule VyasaWeb.ErrorHTMLTest do
   import Phoenix.Template
 
   test "renders 404.html" do
-    assert render_to_string(VyasaWeb.ErrorHTML, "404", "html", []) == "Not Found"
+    assert render_to_string(VyasaWeb.ErrorHTML, "404", "html", []) =~ "Not Found"
   end
 
   test "renders 500.html" do
-    assert render_to_string(VyasaWeb.ErrorHTML, "500", "html", []) == "Internal Server Error"
+    assert render_to_string(VyasaWeb.ErrorHTML, "500", "html", []) =~ "Internal Server Error"
   end
 end
