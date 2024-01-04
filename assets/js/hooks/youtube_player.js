cd8b2972cb2e3dd757ad04d0700d3646140f7ac3
Ritesh Kumar <ritesh@emerald.pink>
2024-01-03 22:58:51 -0500

    Successfully use iframe embed

--- assets/js/hooks/youtube_player.js
+++ assets/js/hooks/youtube_player.js
@@ -4,23 +4,28 @@
 
 YouTubePlayer = {
   mounted() {
-    console.log(">>> mounted YouTubePlayer JS-Hook!!", this)
+    console.log(">>> mounted YouTubePlayer JS-Hook!!", this);
     // 2. This code loads the IFrame Player API code asynchronously.
     let tag = document.createElement("script");
+    const foo = document.getElementById("player");
+    console.log("FOO before: ", foo);
     // tag.crossOrigin = 'anonymous'; /// seems like this will prevent the CORS allow origin from youtube to work correctly since it does the opposite (by allowing origin-only) REF: https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/crossorigin
     tag.src = "https://www.youtube.com/iframe_api";
-    console.log(">>> script tags:", document.getElementsByTagName("script"))
+    console.log(">>> script tags:", document.getElementsByTagName("script"));
     /// ensures that the API script is loaded before any subsequent scripts that depend on it, hence we insert before the first script tag:
     let firstScriptTag = document.getElementsByTagName("script")[0];
     firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-    console.log(">> after insertion, script tags:", document.getElementsByTagName("script"))
-
-    // 3. This function creates an <iframe> (and YouTube player)
-    //    after the API code downloads.
+    console.log(
+      ">> after insertion, script tags:",
+      document.getElementsByTagName("script"),
+    );
+    let iframeInitialiserScript = document.createElement("script");
+    document.body.appendChild(iframeInitialiserScript);
+    const stringifiedScript = `
     let player;
     function onYouTubeIframeAPIReady() {
-      console.log(">>> iframe api ready, time to create iframe...")
+      console.log(">>> iframe api ready, time to create iframe...");
       player = new YT.Player("player", {
         height: "390",
         width: "640",
@@ -35,11 +40,11 @@ YouTubePlayer = {
       });
     }
 
-    console.log(">>> player: ", player)
+    console.log(">>> player: ", player);
 
     // 4. The API will call this function when the video player is ready.
     function onPlayerReady(event) {
-      console.log(">>> player ready")
+      console.log(">>> player ready");
       event.target.playVideo();
     }
 
@@ -56,8 +61,10 @@ YouTubePlayer = {
     function stopVideo() {
       player.stopVideo();
     }
+    `
+    const functionCode = document.createTextNode(stringifiedScript);
+    iframeInitialiserScript.appendChild(functionCode)
   },
-
 };
 
 export default YouTubePlayer;
