84077c99f6c2404a8ef422e98998b03ef696426a
Ritesh Kumar <ritesh@emerald.pink>
2024-01-10 22:34:18 -0500

    Add demonstration of using Floating UI
    Built a custom tooltip here.
    
    Key Takeaways:
    1. added import to root template. UMD import done as described here [1], other imports won't work well.
    2. followed their tutorial on how to setup a custom tooltip, used
    show_verse to demonstrate it. Didn't create a generic template /
    component for it, but I think this is good enough since it shows an
    live-view-integrated version of the floating-ui tutorial as seen here [2]
    3. the middleware is just magic.
    
    [1]: https://floating-ui.com/docs/getting-started#umd
    [2]: https://floating-ui.com/docs/tutorial

--- assets/js/hooks/share_quote.js
+++ assets/js/hooks/share_quote.js
@@ -6,6 +6,7 @@ import { getSharer } from "./web_share.js";
 
 ShareQuoteButton = {
   mounted() {
+    initTooltip();
     let callback = () => console.log("share quote!");
 
     if ("share" in navigator) { // uses webshare api:
@@ -22,6 +23,7 @@ ShareQuoteButton = {
       };
     } else if ("clipboard" in navigator) { // copies to clipboard:
       callback = () => {
+        console.log(">> see me:", {"floating ui:": window})
         const {
           chapter_number: chapterNum,
           verse_number: verseNum,
@@ -37,5 +39,67 @@ ShareQuoteButton = {
     this.el.addEventListener("click", callback);
   },
 };
+
+function showTooltip() {
+  const {tooltip} = getButtonAndTooltip();
+  tooltip.style.display = 'block';
+  alignTooltip();
+}
+
+function hideTooltip() {
+  const {tooltip} = getButtonAndTooltip();
+  tooltip.style.display = '';
+}
+
+const initTooltip = () => {
+  const {button} = getButtonAndTooltip();
+
+  [
+    ['mouseenter', showTooltip],
+    ['mouseleave', hideTooltip],
+    ['focus', showTooltip],
+    ['blur', hideTooltip],
+  ].forEach(([event, listener]) => {
+    button.addEventListener(event, listener);
+  });
+}
+
+const getButtonAndTooltip = () => {
+  const id = "ShareQuoteButton"
+  const button = document.getElementById(id)
+  const tooltip = document.getElementById(`tooltip-${id}`)
+  return {button, tooltip}
+
+}
+
+
+const alignTooltip = () => {
+  const {button, tooltip} = getButtonAndTooltip()
+  console.log(">>> found?", {
+    button,
+    tooltip,
+  })
+  const {
+    computePosition,
+    flip,
+    shift,
+    offset,
+  } = window.FloatingUIDOM;
+  computePosition(button, tooltip, {
+    placement: 'right',
+    // NOTE: order of middleware matters.
+    middleware: [offset(6), flip(), shift({padding: 16})],
+  }).then(({x, y}) => {
+    console.log(">>> computed new position!", {
+      x,
+      y
+    })
+    Object.assign(tooltip.style, {
+      left: `${x}px`,
+      top: `${y}px`,
+    });
+  });
+}
+
 
 export default ShareQuoteButton;
