df3ea6c530e06fc286c9523afa5c763baf0aab01
KosmonautX <avivekbala@gmail.com>
2024-02-13 18:57:03 +0800

    Foundations of the CLI (#24)
    * Foundations of the CLI
    
    * Fixing Finch Behaviour for Escripts
    
    * Wayback Fallback
    
    * File Storage and Chalisa.json done
    
    * Support fork for escripts vs application runtime
    
    ---------
    
    Co-authored-by: ks0m1c_dharma <sakiyamuni@sams.ara>
    Co-authored-by: Ritesh Kumar <ritesh@emerald.pink>

---
+++ lib/vyasa_cli.ex
@@ -0,0 +1,40 @@
+defmodule VyasaCLI do
+    def main(args \\ []) do
+    IO.inspect(args)
+    args
+    |> parse_args()
+    |> response()
+    |> IO.puts()
+  end
+
+
+  defp parse_args([command | ["--" <> _] = args]) do
+    {opts, _, _} =
+      args
+      |> OptionParser.parse(switches: [storage: :string])
+    {command, opts}
+  end
+
+  defp parse_args([command | [arg | _] = args]) do
+    {opts, _, _} =
+      args
+      |> OptionParser.parse(switches: [o: :string, path: :string])
+
+    {command, arg, opts}
+  end
+  defp response({"fetch", "shlokam.org/" <> path, opts}) do
+    Vyasa.Corpus.Engine.Shlokam.run(path, opts)
+  end
+
+  defp response({"fetch", _, _}) do
+  "Unsupported domain
+  Try one of the following:
+  shlokam.org/
+  "
+  end
+
+  defp response(_) do
+  "Command doesnt belong to us "
+  end
+
+end
--- mix.exs
+++ mix.exs
@@ -10,6 +10,7 @@ defmodule Vyasa.MixProject do
       elixirc_options: [
         warnings_as_errors: true
       ],
+      escript: escript(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps()
@@ -24,6 +25,11 @@ defmodule Vyasa.MixProject do
       mod: {Vyasa.Application, []},
       extra_applications: [:logger, :runtime_tools]
     ]
+  end
+
+  # Defining Scripting Env
+  defp escript do
+    [main_module: VyasaCLI]
   end
 
   # Specifies which paths to compile per environment.
@@ -42,7 +48,7 @@ defmodule Vyasa.MixProject do
       {:phoenix_html, "~> 3.3"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "~> 0.20.1"},
-      {:floki, ">= 0.30.0", only: :test},
+      {:floki, ">= 0.30.0"},
       {:phoenix_live_dashboard, "~> 0.8.2"},
       {:esbuild, "~> 0.8", runtime: Mix.env() == :dev},
       {:tailwind, "~> 0.2.0", runtime: Mix.env() == :dev},
@@ -58,6 +64,7 @@ defmodule Vyasa.MixProject do
       {:vix, "~> 0.5"},
       {:kino, "~> 0.12.0"},
       {:cors_plug, "~> 3.0"},
+      {:req, "~> 0.4.8"}
     ]
   end
 
