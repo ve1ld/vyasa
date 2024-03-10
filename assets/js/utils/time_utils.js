89d1915964e5bec6b3d63539595df7cda62e82fc
Ritesh Kumar <ritesh@emerald.pink>
2024-03-10 13:25:21 -0400

    Attempt bugfix for IOS devices.
    Reportedly,
    on iOS devices, regardless of browser, the html5 audio player would
    reach a hung state when the play button would be clicked.
    This was flaky.
    
    Empirically, it was observed that on instances when the hung state
    happens, the formatDisplayTime() fn errored out. This was likely due to
    the face that the Date::toISOString() was receiving Infinity as an
    argument and hence was erroring out.
    
    This fix should account for that case, and hopefully fix the general
    hung state issue on IOS.

--- assets/js/utils/time_utils.js
+++ assets/js/utils/time_utils.js
@@ -7,7 +7,14 @@
  * hh:mm:ss e.g. 00:12:05
  * */
 export const formatDisplayTime = (seconds) => {
-    return new Date(1000 * seconds).toISOString().substring(11, 19)
+  let formatted = null
+  try {
+    formatted = new Date(1000 * seconds).toISOString().substring(11, 19)
+  } catch (e) {
+    console.warn("Errored out when doing time conversions.", e)
+  } finally {
+    return formatted
+  }
 }
 
 export const nowSeconds = () => Math.round(Date.now() / 1000)
