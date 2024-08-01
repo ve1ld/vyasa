d79b816a2fbe5651ec80a51877dfb0781c170d62
Ritesh Kumar <ritesh@emerald.pink>
2024-08-01 09:12:24 +0800

    Revert dummy example

--- assets/js/hooks/audio_player.js
+++ assets/js/hooks/audio_player.js
@@ -195,48 +195,7 @@ AudioPlayer = {
     }
     const newMetadata = this.createMediaMetadata(playback)
     console.log("new metadata", newMetadata)
-    // navigator.mediaSession.metadata = newMetadata
-
-    navigator.mediaSession.metadata = new MediaMetadata({
-        title: "Unforgettable",
-        artist: "Nat King Cole",
-        album: "The Ultimate Collection (Remastered)",
-        artwork: [
-          {
-            src: "https://dummyimage.com/96x96",
-            sizes: "96x96",
-            type: "image/png",
-          },
-          {
-            src: "https://dummyimage.com/128x128",
-            sizes: "128x128",
-            type: "image/png",
-          },
-          {
-            src: "https://dummyimage.com/192x192",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "https://dummyimage.com/256x256",
-            sizes: "256x256",
-            type: "image/png",
-          },
-          {
-            src: "https://dummyimage.com/384x384",
-            sizes: "384x384",
-            type: "image/png",
-          },
-          {
-            src: "https://dummyimage.com/512x512",
-            sizes: "512x512",
-            type: "image/png",
-          },
-        ],
-      });
-
-
-
+    navigator.mediaSession.metadata = newMetadata
 
     // TODO: register action handlers
   },
