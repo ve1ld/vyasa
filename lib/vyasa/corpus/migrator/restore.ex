9dbc123f37ead60d28f900522d3b3037dd762ff3
ks0m1c_dharma <sakiyamuni@sams.ara>
2024-08-17 02:28:07 +0800

    migrator restore

--- lib/vyasa/corpus/migrator/restore.ex
+++ lib/vyasa/corpus/migrator/restore.ex
@@ -3,4 +3,47 @@ defmodule Vyasa.Corpus.Migrator.Restore do
     File.read!(path)
     |> Jason.decode!()
   end
+
+  def run(source) do
+    source
+    |> Enum.map(fn x ->
+      t =
+        x["translations"]
+        |> Enum.group_by(fn m -> {m["type"], m["verse_id"] || m["chapter_no"]} end)
+
+      v =
+        x["voices"]
+        |> Enum.group_by(fn m -> m["chapter_no"] end)
+
+      %{
+        x
+        | "chapters" =>
+          Enum.map(x["chapters"], fn %{"no" => no} = c ->
+            c
+            |> Map.put("translations", t[{"chapters", no}])
+            |> Map.put(
+              "voices",
+            (is_list(v[no]) && v[no] |> Enum.map(fn %{"id" => _} = v -> v end)) || []
+            )
+          end),
+        "verses" =>
+          Enum.map(x["verses"], fn %{"id" => id} = v ->
+            v |> Map.put("translations", t[{"verses", id}])
+          end)
+      }
+    end)
+    |> Enum.map(
+      &(%Vyasa.Written.Source{}
+      |> Vyasa.Written.Source.gen_changeset(&1)
+      |> Vyasa.Repo.insert!())
+    )
+
+    # events are dependent on both verse and voice
+    source
+    |> Enum.map(fn x ->
+      # &1["translations"]
+      x["events"]
+      |> Enum.map(&(&1 |> Vyasa.Medium.create_event()))
+    end)
+  end
 end
