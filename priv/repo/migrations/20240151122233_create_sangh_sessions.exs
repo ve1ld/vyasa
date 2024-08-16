9b23208e50a2d617bf85aa36f3407f8b881be7cc
ks0m1c_dharma <sakiyamuni@sams.ara>
2024-08-16 09:10:32 +0800

    persistent marks

--- priv/repo/migrations/20240151122233_create_sangh_sessions.exs
+++ priv/repo/migrations/20240151122233_create_sangh_sessions.exs
@@ -27,16 +27,13 @@ defmodule Vyasa.Repo.Migrations.CreateSanghSessions do
     create table(:bindings, primary_key: false) do
       add :id, :uuid, primary_key: true
       add :w_type, :string
-      add :field_key, :string
+      add :field_key, :jsonb
       add :verse_id, references(:verses, column: :id, type: :uuid, on_delete: :nothing)
       add :chapter_no, references(:chapters, column: :no, type: :integer, with: [source_id: :source_id])
       add :translation_id, references(:translations, column: :id, type: :uuid, on_delete: :nothing)
       add :comment_id, references(:comments, column: :id, type: :uuid)
-      add :comment_bind_id, references(:comments, column: :id, type: :uuid)
       add :source_id, references(:sources, column: :id, type: :uuid)
       add :window, :jsonb
-
-      timestamps(type: :utc_datetime_usec)
     end
 
     create index(:comments, [:path])
