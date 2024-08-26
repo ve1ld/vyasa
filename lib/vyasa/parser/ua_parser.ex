6353710f2a229c0d7b995fc6742cdd2a45866a41
Ritesh Kumar <ritesh@emerald.pink>
2024-08-26 21:21:36 +0800

    Fix ua_parsing issues
    Now, we do a check using the family values and doing a regex.
    I suspect we'd need to keep updating this if the parsing breaks in
    thefuture. but should be good enough for now.
    
    also the different versions of the ua_parser lib seem to be giving
    different output

--- lib/vyasa/parser/ua_parser.ex
+++ lib/vyasa/parser/ua_parser.ex
@@ -1,13 +1,27 @@
 defmodule Vyasa.UserAgentParser do
   @moduledoc """
-  Uses the external library, ua_parser to add more fine-tuned judgement of
-  what the current user-agent is.
+  Uses the UA Parser library to determine the device type from a user agent string.
   """
-  def parse_user_agent(user_agent) do
-    case UAParser.parse(user_agent) do
-      %UAParser.UA{device: %UAParser.Device{family: "Smartphone"}} -> :mobile
-      %UAParser.UA{device: %UAParser.Device{family: "Tablet"}} -> :tablet
-      _ -> :desktop
+
+  def parse_user_agent(user_agent) when is_binary(user_agent) do
+    ua = UAParser.parse(user_agent)
+
+    cond do
+      is_mobile?(ua) -> :mobile
+      is_tablet?(ua) -> :tablet
+      true -> :desktop
     end
+  end
+
+  def parse_user_agent(_), do: :unknown
+
+  defp is_mobile?(ua) do
+    ua.os.family in ["Android", "iOS", "Windows Phone"] or
+      String.match?(ua.os.family, ~r/Mobile|iP(hone|od)|Android|BlackBerry|IEMobile|Silk/)
+  end
+
+  defp is_tablet?(ua) do
+    (ua.os.family == "iOS" and ua.device.model == "iPad") or
+      (ua.os.family == "Android" and not is_mobile?(ua))
   end
 end
