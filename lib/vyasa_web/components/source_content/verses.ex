2a0a146a7958f73f87358a2488c6b2a575c39c49
Ritesh Kumar <ritesh@emerald.pink>
2024-08-29 12:17:59 +0800

    Create VerseMatrix live_component
    Just comitting this while I make some UI mods next to that.
    The reason why a live_component works here is that I expect to use some
    collapsible state.

--- lib/vyasa_web/components/source_content/verses.ex
+++ lib/vyasa_web/components/source_content/verses.ex
@@ -2,7 +2,6 @@ defmodule VyasaWeb.Content.Verses do
   use VyasaWeb, :live_component
 
   alias Vyasa.Written.{Verse}
-  alias Utils.Struct
 
   @impl true
   def update(params, socket) do
@@ -38,178 +37,27 @@ defmodule VyasaWeb.Content.Verses do
           Back to <%= to_title_case(@src.title) %> Chapters
         </.back>
         <div id="verses" phx-update="stream" phx-hook="HoveRune">
-          <.verse_matrix
+          <.live_component
             :for={{dom_id, %Verse{} = verse} <- @verses}
             id={dom_id}
+            module={VyasaWeb.Content.VerseMatrix}
             verse={verse}
             marks={@marks}
-          >
-            <:edge
-              title={"#{verse.chapter_no}.#{verse.no}"}
-              field={[:body]}
-              verseup={{:big, @src.script}}
-            />
-            <:edge node={hd(verse.translations)} field={[:target, :body_translit]} verseup={:mid} />
-
-            <:edge
-              node={hd(verse.translations)}
-              field={[:target, :body_translit_meant]}
-              verseup={:mid}
-            />
-
-            <:edge node={hd(verse.translations)} field={[:target, :body]} verseup={:mid} />
-          </.verse_matrix>
+            edge={[
+              %{
+                title: "#{verse.chapter_no}.#{verse.no}",
+                field: [:body],
+                verseup: {:big, @src.script}
+              },
+              %{node: hd(verse.translations), field: [:target, :body_translit], verseup: :mid},
+              %{node: hd(verse.translations), field: [:target, :body_translit_meant], verseup: :mid},
+              %{node: hd(verse.translations), field: [:target, :body], verseup: :mid}
+            ]}
+          />
         </div>
         <.back patch={~p"/explore/#{@src.title}"}>
           Back to <%= to_title_case(@src.title) %> Chapters
         </.back>
-      </div>
-    </div>
-    """
-  end
-
-  # ---- CHECKPOINT: all the sangha stuff goes here ----
-  # enum.split() from @verse binding to mark
-  def verse_matrix(assigns) do
-    ~H"""
-    <div class="scroll-m-20 mt-8 p-4 border-b-2 border-brandDark" id={@id}>
-      <dl class="-my-4 divide-y divide-zinc-100">
-        <div :for={elem <- @edge} class="flex gap-4 py-4 text-sm leading-6 sm:gap-8">
-          <dt :if={Map.has_key?(elem, :title)} class="w-1/12 flex-none text-zinc-500">
-            <button
-              phx-click={JS.push("clickVerseToSeek", value: %{verse_id: @verse.id})}
-              class="text-sm font-semibold leading-6 text-zinc-900 hover:text-zinc-700"
-            >
-              <div class="font-dn text-xl sm:text-2xl mb-4">
-                <%= elem.title %>
-              </div>
-            </button>
-          </dt>
-          <div class="relative">
-            <dd
-              verse_id={@verse.id}
-              node={Map.get(elem, :node, @verse).__struct__}
-              node_id={Map.get(elem, :node, @verse).id}
-              field={elem.field |> Enum.join("::")}
-              class={"text-zinc-700 #{verse_class(elem.verseup)}"}
-            >
-              <%= Struct.get_in(Map.get(elem, :node, @verse), elem.field) %>
-            </dd>
-            <div
-              :if={eq_verse_binding(@verse, elem)}
-              id="quick-draft-container"
-              class={[
-                "block mt-4 text-sm text-gray-700 font-serif leading-relaxed
-              opacity-70 transition-opacity duration-300 ease-in-out
-              hover:opacity-100"
-              ]}
-            >
-              <.comment_binding comments={@verse.comments} />
-              <!-- for study https://ctan.math.illinois.edu/macros/latex/contrib/tkz/pgfornament/doc/ornaments.pdf-->
-              <span class="text-primaryAccent flex items-center justify-center">
-                ☙ ——— ›– ❊ –‹ ——— ❧
-              </span>
-              <.drafting marks={@marks} quote={@verse.binding.window && @verse.binding.window.quote} />
-            </div>
-          </div>
-        </div>
-      </dl>
-    </div>
-    """
-  end
-
-  # font by lang here
-  defp verse_class({:big, script}),
-    do: "font-#{script} text-lg sm:text-xl"
-
-  defp verse_class(:mid),
-    do: "font-dn text-m"
-
-  defp eq_verse_binding(verse, elem), do: verse.binding && (verse.binding.node_id == Map.get(elem, :node, verse).id &&
-                   verse.binding.field_key == elem.field)
-
-  def comment_binding(assigns) do
-    # QQ: this elem_id isn't being used explicitly anywhere, was there a purpose for it & can it be removed?
-    # TODO: verify if this custom id assigns can be removed
-    assigns = assigns |> assign(:elem_id, "comment-modal-#{Ecto.UUID.generate()}")
-
-    ~H"""
-    <span
-      :for={comment <- @comments}
-      class="block
-                 before:content-['╰'] before:mr-1 before:text-gray-500
-                 lg:before:content-none
-                 lg:border-l-0 lg:pl-2"
-    >
-      <%= comment.body %> - <b><%= comment.signature %></b>
-    </span>
-    """
-  end
-
-  attr :quote, :string, default: nil
-  attr :marks, :list, default: []
-
-  def drafting(assigns) do
-    assigns = assigns |> assign(:elem_id, "comment-modal-#{Ecto.UUID.generate()}")
-    IO.inspect(assigns, label: "state")
-    ~H"""
-    <div :for={mark <- @marks |> Enum.reverse()} :if={mark.state == :live}>
-      <span
-        :if={!is_nil(mark.binding.window) && mark.binding.window.quote !== ""}
-        class="block
-                 pl-1
-                 ml-5
-                 mb-2
-                 border-l-4 border-primaryAccent
-                 before:mr-5 before:text-gray-500"
-      >
-        <%= mark.binding.window.quote %>
-      </span>
-      <span
-        :if={is_binary(mark.body)}
-        class="block
-                 before:mr-1 before:text-gray-500
-                 lg:before:content-none
-                 lg:border-l-0 lg:pl-2"
-      >
-        <%= mark.body %> - <b><%= "Self" %></b>
-      </span>
-    </div>
-
-    <span
-      :if={!is_nil(@quote) && @quote !== ""}
-      class="block
-                 pl-1
-                 ml-5
-                 mb-2
-                 border-l-4 border-gray-300
-                 before:mr-5 before:text-gray-500"
-    >
-      <%= @quote %>
-    </span>
-
-    <div id="mark-form-container" class="relative">
-      <.form for={%{}} phx-submit="createMark">
-        <input
-          name="body"
-          class="block w-full focus:outline-none rounded-lg border border-gray-300 bg-transparent p-2 pl-5 pr-12 text-sm text-gray-800"
-          placeholder="Write here..."
-          phx-focus={
-            JS.push("verses::focus_toggle_on_quick_mark_drafting", value: %{is_focusing?: true})
-          }
-          phx-blur={
-            JS.push("verses::focus_toggle_on_quick_mark_drafting", value: %{is_focusing?: false})
-          }
-          phx-window-blur={
-            JS.push("verses::focus_toggle_on_quick_mark_drafting", value: %{is_focusing?: false})
-          }
-          phx-keyup="verses::focus_toggle_on_quick_mark_drafting"
-        />
-      </.form>
-      <div class="absolute inset-y-0 right-2 flex items-center">
-        <button class="flex items-center rounded-full bg-gray-200 p-1.5">
-          <.icon name="hero-sun-mini" class="w-3 h-3 hover:text-primaryAccent hover:cursor-pointer" />
-        </button>
       </div>
     </div>
     """
