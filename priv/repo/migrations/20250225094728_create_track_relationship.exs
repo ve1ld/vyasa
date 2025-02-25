f7d1c293892479e2096b835bd7ec53f045bfe48d
ks0m1c_dharma <sakiyamuni@sams.ara>
2025-02-25 17:48:01 +0800

    Create Tracks Relationship

---
+++ priv/repo/migrations/20250225094728_create_track_relationship.exs
@@ -0,0 +1,31 @@
+defmodule Vyasa.Repo.Migrations.CreateTrackRelationship do
+  use Ecto.Migration
+
+  def up do
+    alter table(:tracks) do
+      remove :title
+      add :order, :integer
+      add :event_id, references(:events, column: :id, type: :uuid, on_delete: :nothing)
+      add :trackls_id, references(:tracklists, column: :id, type: :uuid, on_delete: :delete_all)
+    end
+
+
+    create index(:tracks, [:trackls_id])
+    create index(:tracks, [:event_id])
+
+  end
+
+  def down do
+    alter table(:tracks) do
+      add :title, :string
+      remove :order, :integer
+      remove :event_id, references(:events, column: :id, type: :uuid, on_delete: :nothing)
+      remove :trackls_id, references(:tracklists, column: :id, type: :uuid, on_delete: :delete_all)
+    end
+
+    drop index(:tracks, [:trackls_id])
+    drop index(:tracks, [:event_id])
+
+
+  end
+end
